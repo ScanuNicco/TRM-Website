ba70df7119cc15351e12d51e6f028dafc8f5b3c2
scanunicco <Scanuproductions@gmail.com>
2023-02-06 10:35:00 -0500

    Add dynamic logo

--- scripts.js
+++ scripts.js
@@ -6,6 +6,10 @@ function checkNavBar() {
         document.getElementById("navBar").style.boxShadow = "0px 3px 3px rgba(0, 0, 0, .5)";
         document.getElementById("navBar").style.textShadow = "none";
         document.getElementById("homeImage").style.height = "40px";
+        document.getElementById("patht").style.fill = "#000";
+        document.getElementById("pathr").style.fill = "#000";
+        document.getElementById("pathm").style.fill = "#000";
+        document.getElementById("redr").style.fill = "#800";
     } else {
         document.getElementById("navBar").style.background = "transparent";
         document.getElementById("navBar").style.backdropFilter = "none";
@@ -13,6 +17,10 @@ function checkNavBar() {
         document.getElementById("navBar").style.boxShadow = "none";
         document.getElementById("navBar").style.textShadow = "1px 1px 1px rgba(0, 0, 0, .7), 2px 2px 3px rgba(0, 0, 0, .7), 4px 4px 10px rgba(0, 0, 0, .7)";
         document.getElementById("homeImage").style.height = "60px";
+        document.getElementById("patht").style.fill = "#fff";
+        document.getElementById("pathr").style.fill = "#fff";
+        document.getElementById("pathm").style.fill = "#fff";
+        document.getElementById("redr").style.fill = "#fffa";
     }
 }
 /* pretty sure this was only used for the removed easter egg, keeping in case we need it
